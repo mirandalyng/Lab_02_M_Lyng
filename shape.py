41b892bad9323ffbed9feeb3a62cc9a146e648f5
Miranda Lyng <lyngmiranda@gmail.com>
2025-10-27 12:56:47 +0100

    imported abc for use of abstract propertys and methods

--- shape.py
+++ shape.py
@@ -1,25 +1,31 @@
+import math
+from abc import ABC, abstractmethod
+
+
+
 class Shape: 
     def __init__(self, x: float, y: float):
         self.x = x 
         self.y = y 
 
-    
-    def translate(tx: float, ty: float) -> None: 
 
-    
-
-
-
+    """Abstract Propertys are defined"""
     @property
+    @abstractmethod
     def area(self) -> None: 
-        return None
+        pass 
+    """Returnes the area of the shape"""
     
 
     @property 
-    def perimeter(self) -> None: 
-        return None 
+    @abstractmethod 
+    def perimeter(self) -> None:  
+        pass 
+    """Returnes the perimeter of the shape"""
+
+
     
-    
+
 
     def __eq__(self, other) -> Bool: 
 
