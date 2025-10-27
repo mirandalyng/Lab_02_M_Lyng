0c3dc46a330fe5d762156109efda097113f8e39a
Miranda Lyng <lyngmiranda@gmail.com>
2025-10-27 12:38:22 +0100

    added the uml, the operator overlaoding methods and property names in the shape class py

---
+++ shape.py
@@ -0,0 +1,35 @@
+class Shape: 
+    def __init__(self, x: float, y: float):
+        self.x = x 
+        self.y = y 
+
+    
+    def translate(tx: float, ty: float) -> None: 
+
+    
+
+
+
+    @property
+    def area(self) -> None: 
+        return None
+    
+
+    @property 
+    def perimeter(self) -> None: 
+        return None 
+    
+    
+
+    def __eq__(self, other) -> Bool: 
+
+    def __lt__(self, other) -> Bool: 
+
+    def __gt__(self, other) -> Bool: 
+
+    def __le__(self, other) -> Bool:
+    
+    def __ge__(self, other) -> Bool:
+
+
+
